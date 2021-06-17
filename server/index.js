316e147b9bed772fedc32f284dce8053feea3b8b
shug-op <shugis2op@gmail.com>
2021-06-17 10:59:26 -0700

    init

---
+++ server/index.js
@@ -0,0 +1,23 @@
+import cors from "cors";
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+const CONNECTION_URL = process.env.CONNECTION_URL;
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+  )
+  .catch((e) => console.log(e));
+
+mongoose.set("useFindAndModify", false);
