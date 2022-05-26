5116fea80d553a523a2765f552403ef0aee47384
pq43 <n.43.mehta@gmail.com>
2022-05-26 09:26:34 -0700

    pwa again

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -18,7 +18,7 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-serviceWorkerRegistration.unregister();
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
